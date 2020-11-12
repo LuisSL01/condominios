9b1b1b462bff7ef9fdd0a6afa94ce04ae7f630d2
LuisSL01 <lsv0195@gmail.com>
2020-11-12 17:42:10 -0600

    Se implementa redireccionar al menu desde la pagina home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { DataLocalService } from '../../services/data-local.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  user:string="";
+  pass:string="";
+
+  constructor(public alertController:AlertController,
+              private router:Router,
+              private dataLocalService:DataLocalService) { }
 
   ngOnInit() {
+  }
+
+  
+  iniciarSesion(){
+
+    console.log("iniciarSesion");
+
+    console.log('this.user:'+this.user);
+    console.log('this.pass:'+this.pass);
+
+    if(this.user.toLowerCase() === 'test1' 
+                && this.pass.toLowerCase() === 'test1'){    
+      this.dataLocalService.idempresa = 1;//Aqui es cuando se podra setear el id del usuario que se este logueando
+      this.router.navigate(['/inicio']);            
+    }else if(this.user.toLowerCase() === 'test2' 
+                && this.pass.toLowerCase() === 'test2'){      
+      this.dataLocalService.idempresa = 2;//Aqui es cuando se podra setear el id del usuario que se este logueando
+      this.router.navigate(['/inicio']);            
+    }else{      
+      this.presentAlert();
+    }
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Mensaje',
+      subHeader: 'Error de autenticación',
+      message: 'Usuario o contraseña invalidos.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
