9fcbc75972a4bd8fd1fd4ddb3419b0ca4475c209
LuisSL01 <lsv0195@gmail.com>
2021-02-02 17:52:40 -0600

    prueba
    Es una prueba desde mi repo en github desktoo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,6 +13,8 @@ export const environment = {
   formsServiceBaseUrl: 'http://54.177.89.203:8082',  */
  
   
+//Es una prueba desde el local
+
   authServiceBaseUrl: 'http://192.168.2.116:8080',
   coreServiceBaseUrl: 'http://192.168.2.116:8081',
   formsServiceBaseUrl: 'http://192.168.2.116:8082',
