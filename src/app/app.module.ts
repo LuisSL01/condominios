a9c9b41d93a6c2b345b41f49cd2539312e898a12
Eduardo vortex <eduardo.ramirez@erpvortex.com>
2021-03-22 11:21:25 -0600

    Update perfil -e

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,8 @@ import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 import { CallNumber } from '@ionic-native/call-number/ngx';
 
+import { DatePipe } from '@angular/common';
+
 
 /* import { NgxMaskModule, IConfig } from 'ngx-mask' */
 /* export const options: Partial<IConfig> | (() => Partial<IConfig>) = null; */
@@ -58,6 +60,7 @@ import { CallNumber } from '@ionic-native/call-number/ngx';
     BarcodeScanner,
     EmailComposer,
     CallNumber,
+    DatePipe,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
