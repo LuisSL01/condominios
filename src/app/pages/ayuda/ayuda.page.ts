905610cc708ae9a0cd2e7f3abd3d0ce2d8a443ff
Eduardo vortex <eduardo.ramirez@erpvortex.com>
2021-04-15 17:34:27 -0500

    subir log -e

---
+++ src/app/pages/ayuda/ayuda.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { UserData } from '../../providers/user-data';
+import { LogService } from '../../services/log.service';
+
+
+@Component({
+  selector: 'app-ayuda',
+  templateUrl: './ayuda.page.html',
+  styleUrls: ['./ayuda.page.scss'],
+})
+export class AyudaPage implements OnInit {
+
+  idEmpresa: number;
+
+  constructor(
+    public userData: UserData,
+    private logService: LogService,
+  ) {}
+
+
+  ngOnInit() {
+    this.idEmpresa = this.userData.getIdEmpresa();
+  }
+
+  btnEscribir(){
+    var a = 'fui fui fui'
+    this.logService.escribeLog(a)
+  }
+
+  btnLog(){
+    this.logService.compartirLog()
+  }
+
+  btnReinicio(){
+    this.logService.reiniciarLog()
+  }
+
+}
