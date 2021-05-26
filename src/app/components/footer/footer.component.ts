ffe3f932b80cc425d6b64c3f572d8e66417b27c7
Eduardo vortex <eduardo.ramirez@erpvortex.com>
2021-05-26 16:13:50 -0500

    carrucel de publicidad -e

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Publicidad } from 'src/app/models/publicidad.model';
 import { PublicidadService } from '../../services/publicidad.service';
+import { UserData } from '../../providers/user-data';
 
 @Component({
   selector: 'app-footer',
@@ -15,44 +16,67 @@ export class FooterComponent implements OnInit {
   slideOpts = {    
     //El slide sigue mostrando una imagen en blanco
     speed: 1000,
-    loop: true,    
+    loop: false,    
     initialSlide: 0,
     slidesPerView: 1,
     autoHeight: true,
     
     autoplay: {
       disableOnInteraction: false
-    }
-
-
-    ,effect: 'slide',  
+    },
+    effect: 'slide',  
     freeMode: true,
     freeModeSticky: false,
-    spaceBetween: 25
+    spaceBetween: 25,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    }
   };
 
   
   
-  constructor(public publicidadService:PublicidadService) { 
-              this.insertaPub();
+  constructor(public publicidadService:PublicidadService, 
+              private userData:UserData) { 
+              
 
   }
   ngOnInit() {
     /* this.publicidadService.cargarRegistros();     */
+    this.insertaPub();
   }
 
-  insertaPub(){
-      console.log('insertarndo componente spublicidad');    
-      
-      this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve1.png", "https://grupoerve.com/e-sur/"));
-      this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve2.png", "https://grupoerve.com/e-sur/"));
-      this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve3.png", "https://grupoerve.com/rincon-esmeralda/"));
-      this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve4.png", "https://grupoerve.com/vista-reforma/"));
-      this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve5.png", "https://grupoerve.com/vista-reforma/"));
-      this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve6.png", "https://grupoerve.com/rincon-de-la-plata/"));
-      this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve7.png", "https://grupoerve.com/punta-poniente/"));
-      this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve8.png", "https://grupoerve.com/rincon-del-valle/"));
+  async insertaPub(){ 
+    console.log('vamos por la publicidad');  
+    
+    this.publicidadService.getPublicidadAllPorEmpresa(this.userData.getIdEmpresa()).subscribe(
+      data=>{        
+        if(data.status === 200){
+          var publicidades:Publicidad[] = data.result;    
+          console.log('insertando componentes publicidad recuperados');  
+
+          for(var i = 0; i < publicidades.length; i++){
+            this.imagesPublicidad.push(this.generaElementPub(("https://almacenamientonube.s3.us-west-1.amazonaws.com/").concat(publicidades[i].data.pathImage), publicidades[i].data.pathUrl));
+          }
+         
+          //this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve1.png", "https://grupoerve.com/e-sur/"));
+          //this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve2.png", "https://grupoerve.com/e-sur/"));
+          //this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve3.png", "https://grupoerve.com/rincon-esmeralda/"));
+          //this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve4.png", "https://grupoerve.com/vista-reforma/"));
+          //this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve5.png", "https://grupoerve.com/vista-reforma/"));
+          //this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve6.png", "https://grupoerve.com/rincon-de-la-plata/"));
+          //this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve7.png", "https://grupoerve.com/punta-poniente/"));
+          //this.imagesPublicidad.push(this.generaElementPub("https://almacenamientonube.s3.us-west-1.amazonaws.com/App/Banners-app-desarrollos-erve8.png", "https://grupoerve.com/rincon-del-valle/"));
+
+        }else{
+          console.log('Error al recuperar registros de publicidad')
+        }
+      }, err => {                        
+        console.log('Error en el servicio al buscar registros')
+      }
+    );
   }
+
   generaElementPub(_pathImage: string, _pathUrl: string): Publicidad {
     let ob:Publicidad = new Publicidad();    
     const obData = {
@@ -70,7 +94,6 @@ export class FooterComponent implements OnInit {
 
   slidesDidLoad(slides) {
     console.log('slidesDidLoad');
-    
     slides.startAutoplay();
   }
   
